e114e007f04cfc048eac0cff1642c0a2a97b9bfd
Madison Krell <55954812+makrell38@users.noreply.github.com>
2021-10-21 11:17:51 -0500

    Add files via upload

---
+++ nonDominated.py
@@ -0,0 +1,171 @@
+import copy
+from argparse import Namespace, ArgumentParser
+from pathlib import Path
+import pickle
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import math
+
+from non_Dom import nonDom
+from bax.models.simple_gp import SimpleGp
+from bax.models.gpfs_gp import MultiGpfsGp
+from bax.models.stan_gp import get_stangp_hypers
+from bax.acq.acquisition import (
+    BaxAcqFunction, UsBaxAcqFunction, EigfBaxAcqFunction, RandBaxAcqFunction
+)
+from bax.acq.acqoptimize import AcqOptimizer
+from bax.acq.visualize import AcqViz1D
+from bax.util.domain_util import unif_random_sample_domain
+
+import neatplot
+neatplot.set_style("fonts")
+neatplot.update_rc('font.size', 20)
+
+seed = 12
+n_init = 1
+n_iter = 20
+
+
+print(f"*[INFO] Seed: {seed}")
+np.random.seed(seed)
+tf.random.set_seed(seed)
+
+f = lambda x: [np.sin(x[0])*x[1], np.cos(x[1])*x[0]]
+
+# Set algorithm  details
+n_dim = 2
+domain = [[0, 1], [0, np.pi*.5]] 
+len_path = 150
+
+x_path = unif_random_sample_domain(domain, len_path)
+algo = nonDom({"x_path": x_path}, 2)
+
+# Get ground truth algorithm output
+algo_gt = nonDom({"x_path": x_path, "name": "groundtruth"}, 2)
+exepath_gt, output_gt = algo_gt.run_algorithm_on_f(f)
+print(f"Algorithm ground truth output is:\n{output_gt}")
+
+# Set data for model
+data = Namespace()
+data.x = unif_random_sample_domain(domain, n_init)
+data.y = [f(x) for x in data.x]
+
+gp_params = {"ls": 2.5, "alpha": 20.0, "sigma": 1e-2, "n_dimx": n_dim}
+modelclass = MultiGpfsGp
+
+# Set acquisition details
+acqfn_params = {"acq_str": "exe", "n_path": 100, "crop": True}
+#acq_cls = BaxAcqFunction
+acq_cls = EigfBaxAcqFunction
+
+# Set acqopt details
+n_acqopt = 1500
+
+# Set up results directory
+results_dir = Path("nonDom_pareto")
+results_dir.mkdir(parents=True, exist_ok=True)
+
+# Set up img directory
+img_dir = results_dir /  f'images_{seed}'
+img_dir.mkdir(parents=True, exist_ok=True)
+
+"""
+fig, ax = plt.subplots(figsize=(6, 6))
+
+x_arr = np.array(x_path)
+
+
+ax.plot(x_arr[:, 0], x_arr[:, 1], 'o', color='k', markersize=3)
+
+x_output_arr = np.array(output_gt)
+ax.plot(
+        x_output_arr[:, 0],
+        x_output_arr[:, 1],
+        '*',
+        marker='D',
+        markersize=15,
+        color='c',
+        markeredgecolor='black',
+        markeredgewidth=0.05,
+        zorder=1
+    )
+
+#ax.set(xlim=[0,1.5], ylim=[0,.6])
+ax.set_title("Pareto Frontier")
+ax.set_xlabel("X[1]")
+ax.set_ylabel("X[2]")
+
+img_path = img_dir / f'nondom_X'
+neatplot.save_figure(str(img_path), 'pdf')
+"""
+# Run BAX loop
+for i in range(n_iter):
+    # Set model
+    model = modelclass({"name": "MultiGpfsGp", "n_dimy": 2, "gp_params":gp_params}, data)
+
+    # Set and optimize acquisition function
+    acqfn = acq_cls(acqfn_params, model, algo)
+    x_test = unif_random_sample_domain(domain, n=n_acqopt)
+    acqopt = AcqOptimizer({"x_batch": x_test})
+    x_next = acqopt.optimize(acqfn)
+
+
+    # Print iter info
+    print(f"Acqopt x_next = {x_next}")
+    print(f"output_list[0] = {acqfn.output_list[0]}")
+    print(f"Finished iter i = {i}")
+    
+    # Plot
+    fig, ax = plt.subplots(figsize=(6, 6))
+    # -- plot function contour
+    grid = 0.1
+
+    # -- plot top_k
+    nondom_arr = np.array(output_gt)
+    ax.plot(
+        nondom_arr[:, 0],
+        nondom_arr[:, 1],
+        '*',
+        marker='D',
+        markersize=15,
+        color='c',
+        markeredgecolor='black',
+        markeredgewidth=0.05,
+        zorder=1
+    )
+
+    # -- plot x_path
+    x_path_arr = np.array(x_path)
+    ax.plot(x_path_arr[:, 0], x_path_arr[:, 1], 'o', color='k', markersize=3)
+    # -- plot observations
+    for x in data.x:
+        ax.scatter(x[0], x[1], color='r', s=80)
+    # -- plot x_next
+    ax.scatter(x_next[0], x_next[1], color='g', s=80, zorder=10)
+    # -- plot estimated output
+    for out in acqfn.output_list:
+        out_arr = np.array(out.x)
+        ax.plot(
+            out_arr[:,0], out_arr[:,1], 's', markersize=8, color='m', alpha=0.02
+        )
+    # -- lims, labels, titles, etc
+    #ax.set(xlim=domain[0], ylim=domain[1])
+    ax.set_title("BAX with Multi-objective Optimization Algorithm")
+    ax.set_xlabel("X[1]")
+    ax.set_ylabel("X[2]")
+
+    # Save plot
+    img_path = img_dir / f'nondom_{i}'
+    neatplot.save_figure(str(img_path), 'pdf')
+
+    # Query function, update data
+    y_next = (f(x_next))
+    data.x.append(x_next)
+    data.y.append(y_next)
+
+
+file_str = f"nondom_{seed}.pkl"
+with open(results_dir / file_str, "wb") as handle:
+    print(f"Saved results file: {results_dir}/{file_str}")
---
+++ non_Dom.py
@@ -0,0 +1,147 @@
+from argparse import Namespace
+import copy
+import heapq
+import numpy as np
+import math
+import random
+
+from bax.alg.algorithms import Algorithm
+from bax.util.misc_util import dict_to_namespace
+from bax.util.graph import Vertex, backtrack_indices, edges_of_path, jaccard_similarity
+
+class nonDom(Algorithm):
+    """Base class for a BAX Algorithm"""
+    def __init__(
+        self,
+        params=None,
+        objectives=None,
+        verbose=True,
+    ):
+        super().__init__(params, verbose)
+
+        self.objectives = objectives
+
+    def set_params(self, params):
+        """Set self.params, the parameters for the algorithm."""
+        super().set_params(params)
+        params = dict_to_namespace(params)
+        self.params.name = getattr(params, "name", "nonDom")
+        self.params.x_path = getattr(params, "x_path", [])
+
+    def initialize(self):
+        """Initialize algorithm, reset execution path."""
+        self.exe_path = Namespace(x=[], y=[])
+
+    def get_next_x(self):
+        """
+        Given the current execution path, return the next x in the execution path. If
+        the algorithm is complete, return None.
+        """
+        len_path = len(self.exe_path.x)
+        x_path = self.params.x_path
+        next_x = x_path[len_path] if len_path < len(x_path) else None
+        #print("next_x", next_x)
+        return next_x
+
+    def run_algorithm_on_f(self, f):
+        """
+        Run the algorithm by sequentially querying function f. Return the execution path
+        and output.
+        """
+        self.initialize()
+
+        nonDomarr = []
+        y_path = [f(x) for x in self.params.x_path]
+        nonDomarr.append([valY[0] for valY in y_path])
+        nonDomarr = np.argsort(nonDomarr[0])
+        ret = set()
+        ret.add(nonDomarr[len(nonDomarr)-1])
+        to_remove = set()
+        to_add = set()
+        for i in range(len(nonDomarr)-2,-1,-1):
+            y = y_path[nonDomarr[i]]
+            add = True
+            for s in ret:
+                val = self.params.x_path[s]
+                test_y = f(val)
+                if self.dominates(test_y, y):
+                    add = False
+                    break
+                if self.dominates(y, test_y):
+                    to_remove.add(s)
+            if add:
+                to_add.add(nonDomarr[i])
+            for r in to_remove:
+                ret.remove(r)
+            for a in to_add:
+                ret.add(a)
+        
+        output = []
+        for s in ret:
+            output.append(self.params.x_path[s])
+        return self.exe_path, output
+
+    def dominates(self,p1, p2):
+        objectives = self.objectives
+        better = False
+        for i in range(objectives):
+            if p1[i] < p2[i]:
+                return False
+            if p1[i] > p2[1]:
+                better = True
+        return better
+
+    def get_exe_path_nondom(self):
+        """Return the index of the optimal point in execution path."""
+        
+        nonDomarr = []
+        nonDomarr.append([valY[0] for valY in self.exe_path.y])
+        nonDomarr = np.argsort(nonDomarr[0])
+        ret = set()
+        ret.add(nonDomarr[len(nonDomarr)-1])
+        to_remove = set()
+        to_add = set()
+        for i in range(len(nonDomarr)-2,-1,-1):
+            y = self.exe_path.y[nonDomarr[i]]
+            add = True
+            for s in ret:
+                test_y = self.exe_path.y[s]
+                if self.dominates(test_y, y):
+                    add = False
+                    break
+                if self.dominates(y, test_y):
+                    to_remove.add(s)
+            if add:
+                to_add.add(nonDomarr[i])
+            for r in to_remove:
+                ret.remove(r)
+            for a in to_add:
+                ret.add(a)
+        return ret
+
+
+    def get_exe_path_crop(self):
+        """
+        Return the minimal execution path for output, i.e. cropped execution path,
+        specific to this algorithm.
+        """
+        nonDom_ind = self.get_exe_path_nondom()
+        exe_path_crop = Namespace()
+        exe_path_crop.x = [self.exe_path.x[idx] for idx in nonDom_ind]
+        exe_path_crop.y = [self.exe_path.y[idx] for idx in nonDom_ind]
+        #print("EXE_PATHCROP", exe_path_crop)
+        return exe_path_crop
+        #return self.exe_path
+
+
+    def get_copy(self):
+        """Return a copy of this algorithm."""
+        return copy.deepcopy(self)
+    
+    def get_output(self):
+        """Return best path."""
+        nonDom_ind = self.get_exe_path_nondom()
+        ret = Namespace()
+        ret.x = [self.exe_path.x[idx] for idx in nonDom_ind]
+        ret.y = [self.exe_path.y[idx] for idx in nonDom_ind]
+        return ret
