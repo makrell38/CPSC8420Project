bcf33d82d85acb947ac810d2ffa8e8f0e3672c6c
Madison Krell <55954812+makrell38@users.noreply.github.com>
2021-10-21 13:49:40 -0500

    Update nonDominated.py

--- nonDominated.py
+++ nonDominated.py
@@ -44,7 +44,7 @@ len_path = 150
 x_path = unif_random_sample_domain(domain, len_path)
 algo = nonDom({"x_path": x_path}, 2)
 
-# Get ground truth algorithm output
+# Run algorithm traditionally
 algo_gt = nonDom({"x_path": x_path, "name": "groundtruth"}, 2)
 exepath_gt, output_gt = algo_gt.run_algorithm_on_f(f)
 print(f"Algorithm ground truth output is:\n{output_gt}")
@@ -57,22 +57,20 @@ data.y = [f(x) for x in data.x]
 gp_params = {"ls": 2.5, "alpha": 20.0, "sigma": 1e-2, "n_dimx": n_dim}
 modelclass = MultiGpfsGp
 
-# Set acquisition details
 acqfn_params = {"acq_str": "exe", "n_path": 100, "crop": True}
 acq_cls = BaxAcqFunction
 #acq_cls = EigfBaxAcqFunction
 
-# Set acqopt details
 n_acqopt = 1500
 
-# Set up results directory
+# Set up directories for plots
 results_dir = Path("nonDom_pareto")
 results_dir.mkdir(parents=True, exist_ok=True)
 
-# Set up img directory
 img_dir = results_dir /  f'images_{seed}'
 img_dir.mkdir(parents=True, exist_ok=True)
 
+# uncomment out this section to plot traditional algorithm
 """
 fig, ax = plt.subplots(figsize=(6, 6))
 
@@ -102,7 +100,8 @@ ax.set_ylabel("X[2]")
 img_path = img_dir / f'nondom_X'
 neatplot.save_figure(str(img_path), 'pdf')
 """
-# Run BAX loop
+
+# Run BAX
 for i in range(n_iter):
     # Set model
     model = modelclass({"name": "MultiGpfsGp", "n_dimy": 2, "gp_params":gp_params}, data)
@@ -113,8 +112,6 @@ for i in range(n_iter):
     acqopt = AcqOptimizer({"x_batch": x_test})
     x_next = acqopt.optimize(acqfn)
 
-
-    # Print iter info
     print(f"Acqopt x_next = {x_next}")
     print(f"output_list[0] = {acqfn.output_list[0]}")
     print(f"Finished iter i = {i}")
@@ -124,7 +121,6 @@ for i in range(n_iter):
     # -- plot function contour
     grid = 0.1
 
-    # -- plot top_k
     nondom_arr = np.array(output_gt)
     ax.plot(
         nondom_arr[:, 0],
@@ -138,27 +134,24 @@ for i in range(n_iter):
         zorder=1
     )
 
-    # -- plot x_path
     x_path_arr = np.array(x_path)
     ax.plot(x_path_arr[:, 0], x_path_arr[:, 1], 'o', color='k', markersize=3)
-    # -- plot observations
+
     for x in data.x:
         ax.scatter(x[0], x[1], color='r', s=80)
-    # -- plot x_next
+
     ax.scatter(x_next[0], x_next[1], color='g', s=80, zorder=10)
-    # -- plot estimated output
+
     for out in acqfn.output_list:
         out_arr = np.array(out.x)
         ax.plot(
             out_arr[:,0], out_arr[:,1], 's', markersize=8, color='m', alpha=0.02
         )
-    # -- lims, labels, titles, etc
-    #ax.set(xlim=domain[0], ylim=domain[1])
+
     ax.set_title("BAX with Multi-objective Optimization Algorithm")
     ax.set_xlabel("X[1]")
     ax.set_ylabel("X[2]")
 
-    # Save plot
     img_path = img_dir / f'nondom_{i}'
     neatplot.save_figure(str(img_path), 'pdf')
 
