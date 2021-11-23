d656466562a8f440aa827332b76cc44617ab4fb2
Madison Krell <55954812+makrell38@users.noreply.github.com>
2021-11-23 12:37:01 -0500

    Phase3 Code

---
+++ model.py
@@ -0,0 +1,154 @@
+from functools import partial
+from typing import Optional
+import tensorflow as tf
+from gpflow import Parameter, config
+from gpflow.models import GPModel
+from gpflow.models.model import MeanAndVariance
+from gpflow.models.training_mixins import InputData, RegressionData, InternalDataTrainingLossMixin
+from gpflow.models.util import data_input_to_tensor
+from gpflow.utilities import positive
+from gpflow_sampling import sampling, covariances
+from gpflow_sampling.sampling.core import AbstractSampler, CompositeSampler
+from gpflow.base import TensorLike
+
+from bayesian_algorithm_execution.bax.models.pgps.pssgp.kalman.parallel import pkf, pkfs
+from bayesian_algorithm_execution.bax.models.pgps.pssgp.kalman.sequential import kf, kfs
+
+
+def _merge_sorted(a, b, *args):
+    """
+    Merge sorted arrays efficiently, inspired by https://stackoverflow.com/a/54131815
+
+    Parameters
+    ----------
+    a: tf.Tensor
+        Sorted tensor for ordering
+    b: tf.Tensor
+        Sorted tensor for ordering
+    args: list of tuple of tf.Tensor
+            Some data ordered according to a and b that need to be merged whilst keeping the order.
+
+
+    Returns
+    -------
+    cs: list of tf.Tensor
+        Merging of a_x and b_x in the right order.
+
+    """
+    with tf.name_scope("merge_sorted"):
+        assert len(a.shape) == len(b.shape) == 1
+        a_shape, b_shape = tf.shape(a)[0], tf.shape(b)[0]
+        c_len = tf.shape(a)[0] + tf.shape(b)[0]
+        if a_shape < b_shape:
+            a, b = b, a
+            a_shape, b_shape = tf.shape(a)[0], tf.shape(b)[0]
+            args = tuple((j, i) for i, j in args)
+        b_indices = tf.range(b_shape, dtype=tf.int32) + tf.searchsorted(a, b)
+        a_indices = tf.ones((c_len,), dtype=tf.bool)
+        a_indices = tf.tensor_scatter_nd_update(a_indices, b_indices[:, None], tf.zeros_like(b_indices, tf.bool))
+        c_range = tf.range(c_len, dtype=tf.int32)
+        a_mask = tf.boolean_mask(c_range, a_indices)[:, None]
+
+        def _inner_merge(u, v):
+            c = tf.concat([u, v], 0)
+            c = tf.tensor_scatter_nd_update(c, b_indices[:, None], v)
+            c = tf.tensor_scatter_nd_update(c, a_mask, u)
+            return c
+
+        return (_inner_merge(a, b),) + tuple(_inner_merge(i, j) for i, j in args)
+
+
+class StateSpaceGP(GPModel, InternalDataTrainingLossMixin):
+    def __init__(self,
+                 data: RegressionData,
+                 kernel,
+                 noise_variance: float = 1.0,
+                 parallel=False,
+                 max_parallel=10000
+                 ):
+        self.noise_variance = Parameter(noise_variance, transform=positive())
+        ts, ys = data_input_to_tensor(data)
+        super().__init__(kernel, None, None, num_latent_gps=ys.shape[-1])
+        self.data = ts, ys
+        filter_spec = kernel.get_spec(ts.shape[0])
+        filter_ys_spec = tf.TensorSpec((ts.shape[0], 1), config.default_float())
+        smoother_spec = kernel.get_spec(None)
+        smoother_ys_spec = tf.TensorSpec((None, 1), config.default_float())
+
+        if not parallel:
+            self._kf = tf.function(partial(kf, return_loglikelihood=True, return_predicted=False),
+                                   input_signature=[filter_spec, filter_ys_spec])
+            self._kfs = tf.function(kfs, input_signature=[smoother_spec, smoother_ys_spec])
+        else:
+            self._kf = tf.function(partial(pkf, return_loglikelihood=True, max_parallel=ts.shape[0]),
+                                   input_signature=[filter_spec, filter_ys_spec])
+            self._kfs = tf.function(partial(pkfs, max_parallel=max_parallel),
+                                    input_signature=[smoother_spec, smoother_ys_spec])
+
+    def set_paths(self, paths) -> AbstractSampler:
+        self._paths = paths
+        return paths
+
+    def generate_paths(self,
+                     num_samples: int,
+                     num_bases: int = None,
+                     prior: AbstractSampler = None,
+                     sample_axis: int = None,
+                     **kwargs) -> CompositeSampler:
+
+        if prior is None:
+            prior = sampling.priors.random_fourier(self.kernel,
+                                                    num_bases=num_bases,
+                                                    sample_shape=[num_samples],
+                                                    sample_axis=sample_axis)
+        elif num_bases is not None:
+            assert prior.sample_shape == [num_samples]
+
+        ts, y = self.data
+        self._make_model(ts)
+        #diag = tf.convert_to_tensor(self.likelihood.variance)
+        diag = tf.convert_to_tensor(self.noise_variance)
+        return sampling.decoupled(self.kernel,
+                                prior,
+                                *self.data,
+                                mean_function=self.mean_function,
+                                diag=diag,
+                                sample_axis=sample_axis,
+                                **kwargs)
+
+    def _make_model(self, ts):
+        with tf.name_scope("make_model"):
+            #R = self.noise_variance
+            R = tf.reshape(self.noise_variance, (1, 1))
+            ssm = self.kernel.get_ssm(ts, R)
+        return ssm
+
+    def predict_f(
+            self, Xnew: TensorLike, num_samples: Optional[int] = None, full_cov: bool = True, full_output_cov: bool = True, **kwargs
+    ) -> MeanAndVariance:
+        ts, ys = self.data
+        Xnew = tf.convert_to_tensor(Xnew, dtype=config.default_float())
+        squeezed_ts = tf.squeeze(ts,1)
+        squeezed_Xnew = tf.squeeze(Xnew,1)
+        #squeezed_ys = tf.squeeze(ys)
+        float_ys = float("nan") * tf.ones((Xnew.shape[0], ys.shape[1]), dtype=ys.dtype)
+        all_ts, all_ys, all_flags = _merge_sorted(squeezed_ts, squeezed_Xnew,
+                                                  (ys, float_ys),
+                                                  (tf.zeros_like(squeezed_ts, dtype=tf.bool),
+                                                   tf.ones_like(squeezed_Xnew, dtype=tf.bool)))
+        #  this merging is equivalent to using argsort but uses O(log(T)) operations instead.
+        ssm = self._make_model(all_ts[:, None])
+        sms, sPs = self._kfs(ssm, all_ys)
+        res = tf.boolean_mask(sms, all_flags, 0), tf.boolean_mask(sPs, all_flags, 0)
+        return tf.linalg.matvec(ssm.H, res[0]), tf.linalg.diag_part(tf.linalg.matmul(ssm.H,
+                                                                                     tf.linalg.matmul(res[1],
+                                                                                                      ssm.H,
+                                                                                                      transpose_b=True)))
+
+    def maximum_log_likelihood_objective(self) -> tf.Tensor:
+        ts, Y = self.data
+        ssm = self._make_model(ts)
+        fms, fPs, ll = self._kf(ssm, Y)
+        return ll
+
+    
---
+++ pssgpTest.py
@@ -0,0 +1,194 @@
+import numpy as np
+import tensorflow as tf
+from pathlib import Path
+import math
+import matplotlib.pyplot as plt
+from numpy.linalg import cholesky
+from argparse import Namespace, ArgumentParser
+import gpflow
+from gpflow.utilities import print_summary
+from gpflow import default_float
+
+import sys
+
+from pargpfsgp import ParGpfsGp
+from model import StateSpaceGP
+import bayesian_algorithm_execution.neatplot as neatplot
+
+from bayesian_algorithm_execution.bax.models.gpfs_gp import GpfsGp
+from bayesian_algorithm_execution.bax.alg.algorithms import TopK
+from bayesian_algorithm_execution.bax.models.pgps.pssgp.kernels.matern import Matern52
+from bayesian_algorithm_execution.bax.models.pgps.pssgp.kernels.rbf import RBF
+from bayesian_algorithm_execution.bax.acq.acquisition import (
+    BaxAcqFunction, UsBaxAcqFunction, EigfBaxAcqFunction, RandBaxAcqFunction
+)
+from bayesian_algorithm_execution.bax.acq.acqoptimize import AcqOptimizer
+
+def unif_random_sample_domain(domain, n=1):
+    """Draws a sample uniformly at random from domain (a list of tuple bounds)."""
+    list_of_arr_per_dim = [np.random.uniform(dom[0], dom[1], n) for dom in domain]
+    list_of_list_per_sample = [list(l) for l in np.array(list_of_arr_per_dim).T]
+    return list_of_list_per_sample
+
+print(tf.__version__)
+print(tf.config.list_physical_devices())
+
+neatplot.set_style("fonts")
+neatplot.update_rc('font.size', 20)
+plt.rc('text', usetex=False)
+
+seed = 12
+np.random.seed(seed)
+tf.random.set_seed(seed)
+
+
+
+#f = lambda x: 2*x
+f = lambda x: math.sin(x*math.pi) + math.sin(2 * math.pi * x) + math.sin(3 * math.pi * x)
+#f = lambda x_list: np.sum([f_0(x) for x in x_list])
+#f_0 = lambda x: 2 * np.abs(x) * np.sin(x)
+#f = lambda x_list: np.sum([f_0(x) for x in x_list])
+n_dim = 1
+domain = [[1, 10]] * n_dim
+dtype = default_float()
+
+len_path = 150
+
+X = unif_random_sample_domain(domain, len_path)
+X = np.asarray(X)
+X = X.reshape(-1,n_dim)
+
+Y = [f(x) for x in X]
+#X = X.tolist()
+
+Y = np.asarray(Y)
+#Y = Y.flatten()
+rng = np.random.RandomState(12)
+#Y = Y + np.sqrt(1e-2**2) * rng.normal(Y, math.sqrt((1e-2)**2), (Y.shape[0],1)).astype(dtype)
+#
+
+#Y = Y.reshape(-1,1)
+
+
+
+k = 10
+
+algo = TopK({"x_path": X, "k": k})
+
+algo_gt = TopK({"x_path": X, "k": k, "name": "groundtruth"})
+exepath_gt, output_gt = algo_gt.run_algorithm_on_f(f)
+print(f"Algorithm ground truth output is:\n{output_gt}")
+
+data = Namespace()
+#data.x = np.linspace(0,max_val, 1, dtype=dtype)
+data.x = unif_random_sample_domain(domain, 1)
+data.x = np.asarray(data.x)
+data.x = data.x.reshape(-1,1)
+data.y = [f(x) for x in data.x]
+#data.x = data.x.tolist()
+
+data.y = np.asarray(data.y)
+#data.y = data.y.reshape(-1,1)
+
+
+gp_params = {"ls": 2.5, "alpha": 20.0, "sigma": 1e-2, "n_dimx": n_dim}
+modelclass = ParGpfsGp
+
+acqfn_params = {"acq_str": "exe", "n_path": 1, "crop": True}
+acq_cls = BaxAcqFunction
+
+n_acqopt = 1
+
+results_dir = Path("parallel_topk_results")
+results_dir.mkdir(parents=True, exist_ok=True)
+
+img_dir = results_dir /  f'images'
+img_dir.mkdir(parents=True, exist_ok=True)
+
+for i in range(1):
+    # Set model
+    model = modelclass(gp_params, data)
+
+    # Set and optimize acquisition function
+    acqfn = acq_cls(acqfn_params, model, algo)
+    x_test = unif_random_sample_domain(domain, n=n_acqopt)
+    #x_test = np.linspace(0,max_val, n_acqopt, dtype=dtype)
+    #x_test = x_test.reshape(-1,1)
+    acqopt = AcqOptimizer({"x_batch": x_test})
+    x_next = acqopt.optimize(acqfn)
+
+    # Print iter info
+    print(f"Acqopt x_next = {x_next}")
+    print(f"output_list[0] = {acqfn.output_list[0]}")
+    print(f"Finished iter i = {i}")
+
+    # Plot
+    fig, ax = plt.subplots(figsize=(6, 6))
+    # -- plot function contour
+    #grid = 0.1
+    #xpts = np.arange(domain[0][0], domain[0][1], grid)
+    #ypts = np.arange(domain[1][0], domain[1][1], grid)
+    #X, Y = np.meshgrid(xpts, ypts)
+    #Z = f_vec(X, Y)
+    #ax.contour(X, Y, Z, 20, cmap=cm.Greens_r, zorder=0)
+    # -- plot top_k
+    """
+    topk_arr = np.array(output_gt.x)
+    y_arr = [f(x[0]) for x in topk_arr]
+
+    ax.plot(
+        topk_arr[:, 0],
+        y_arr,
+        '*',
+        marker='*',
+        markersize=30,
+        color='gold',
+        markeredgecolor='black',
+        markeredgewidth=0.05,
+        zorder=1
+    )
+    # -- plot x_path
+    #x_path_arr = np.array(x_path)
+    #ax.plot(x_path_arr[:, 0], x_path_arr[:, 1], '.', color='#C0C0C0', markersize=8)
+    ax.plot(X, Y, '.', color='#C0C0C0', markersize=8)
+    # -- plot observations
+    for x in data.x:
+        ax.scatter(x[0], f(x[0]), color=(0, 0, 0, 1), s=80)
+    # -- plot x_next
+    ax.scatter(x_next[0], f(x_next[0]), color='deeppink', s=80, zorder=10)
+    # -- plot estimated output
+    for out in acqfn.output_list:
+        out_arr = np.array(out.x)
+        y_out_arr = [f(x[0]) for x in out_arr]
+        ax.plot(
+            out_arr[:, 0], y_out_arr, 's', markersize=8, color='blue', alpha=0.02
+        )
+    # -- lims, labels, titles, etc
+    #ax.set(xlim=domain[0], ylim=domain[1])
+    ax.set_title("Parallel InfoBAX with Top-$k$ Algorithm")
+
+    # Save plot
+    img_path = img_dir / f'topk_{i}'
+    neatplot.save_figure(str(img_path), 'pdf')
+    
+    # Query function, update data
+
+    y_next = f(x_next[0])
+
+    data.x = np.append(data.x, [x_next])
+    data.y = np.append(data.y, [y_next])
+    data.x = data.x.reshape(-1,1)
+    #data.x = data.x.tolist()
+    #data.y = data.y.reshape(-1,1)
+
+    #data.x.append(x_next)
+    #data.y.append(y_next)
+
+#results.data = data
+
+# Pickle results
+file_str = f"topk.pkl"
+with open(results_dir / file_str, "wb") as handle:
+    #pickle.dump(results, handle)
+    print(f"Saved results file: {results_dir}/{file_str}")
+    """
